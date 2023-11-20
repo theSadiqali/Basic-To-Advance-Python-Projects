c6bc3a11dc5e9aacd5077ac73b679fe03a25a241
Sadiq Ali <142330669+theSadiqali@users.noreply.github.com>
2023-11-20 18:11:27 +0500

    PYTHON TUTORIAL 2_Print Hello world and variables.py
    Create PYTHON TUTORIAL 2_Print Hello world and variables.py

---
+++ PYTHON TUTORIAL 2_Print Hello world and variables.py
@@ -0,0 +1,90 @@
+""" ------------ PYTHON TUTORIAL 2 ----------------  """
+
+# - Python is a widely used programming language for telling computers what to do.
+# - It's well-known for being easy to understand and write, making it popular among programmers.
+# - People use Python for various tasks such as creating websites, analyzing data, developing games, and teaching computers to do smart tasks like understanding speech or recognizing images.
+# - Python is like a toolbox with many tools inside, helping programmers work quickly and easily.
+# - Many companies seek people who know Python because it's versatile and applicable in various job roles.
+
+#let` Start coding 
+
+print("Hello! Sadiq Ali")
+#try it by yourself
+#print differnet things like your name and hello world etc
+#let me show u how you can save this 
+
+#ctrl + S
+
+#Comment:
+"""single line comment and multiline comment
+Actually python has different way to do comment. let me show you how you can do this. Multiline comment"""
+#single line comment
+"""Multi
+line
+comment"""
+
+#Variable:
+#In python variable are container to storing data values.
+
+#just like this
+
+x = 1
+y = 2
+z = 3
+#ADDING
+s = x+y+z   #storing in (s)
+print(f"x VALUE: {x} ")
+print(f"y VALUE: {y}")
+print(f"z VALUE: {z}")
+print(f"Result: {s}") 
+#PYTHON IS SO SIMPLE AND EASY 
+
+#let me tell you something interesting 
+#VARIABLE ARE CASE SENSITIVE c and C 
+#let me show you 
+C = "SADIQ"
+c = "sadiqali"
+print(c)
+print(C)
+#and you know double qoutes and single qoutes are same 
+m = "S"
+m = 's'
+
+#And we have legal variable and illegal variable in python 
+myvar = "sadiq"
+my_var = "sadiq"
+_my_var = "sadiq"
+MYVAR = "sadiq"
+myvar1 = "sadiq"
+
+#ILLEGAL VARIABLE 
+"""1myvar = "sadiq"
+my-var = "sadiq"
+my var = "sadiq"""
+print("\n\n\n")
+#Many Values to Multiple variables 
+#Actually allow you to assign values to multiple variables in one line.
+names = "sadiq", "sadiq1", "sadiq2"
+q, w, e = names
+print (q)
+print(w)
+print(e)
+
+
+#so now let me tell you about GLOBAL VARIABLE
+#DO NO`T WORRY . WE WILL BE USING A SMALL FUNCTION, SO IT`S NOT A BIG DEAL. I WILL TEACH #YOU THAT LATER
+#GLOBAL VARIABLE:
+"""GLOBALE VARIABLE CAN BE USED BY EVERYONE, BOTH INSIDE AND OUTSIDE OF A FUNCTION"""
+
+def glbl():
+    global abc
+    abc = 'Alphabets'
+    
+glbl()
+
+print(f"Global variable inside a function and outside a function: {abc}")
+#For today its enough. I will teach you next python data types
+#Data types:
+#thank you soo much for watching this video 
+#please like and subscribe this channel.
+#BYE
