b16d533c7df182d64115d5cc47dad30312ffd0a7
Sadiq Ali <142330669+theSadiqali@users.noreply.github.com>
2023-11-27 16:16:08 +0500

    PYTHON TUTORIAL 3.py
    python tutorial 3

---
+++ PYTHON TUTORIAL 3.py
@@ -0,0 +1,143 @@
+            #=========================#
+            #        Tutorial 3       #
+            #-------------------------#
+            #        YOUTUBE          #
+            #   thesadiqali channel   #
+            #  ====================== # 
+            #   PYTHON Tutorial       #
+            #   BASIC TO ADVANCE      #
+            #=========================#
+
+https://www.youtube.com/@Thesadiqali11
+playlist: https://www.youtube.com/playlist?list=PLpzrQqmQAcrXOoA3VSZaVOQ7fzMG0_Do9
+
+'''COURSE SYLLABUS''' 
+# 1 intro to python 
+# 2 strings in python 
+# 3 lists in python 
+# 4 Tuples in python 
+# 5 Sets in Python
+# 6 Dictionaries in Python 
+# 7 Conditions in Python 
+# 8 Loops in Python
+# 9 Functions in Python
+# 10 Exception Handling in Python
+# 11 Built-in Functions in Python 
+# 12 Classes and Objects in Python 
+# 13 Reading Files in Python 
+# 14 Writing Files in Python
+# 15 String Operators and Functions in Python 
+# 16 Arrays in Python
+# 17 Lambda Functions in Python
+# 18 Math Module Functions in Python 
+# 19 List Comprehension in Python 
+# 20 Decorators in Python Generators in Python
+# PYTHON BASIC AND ADVANCE PROJECTS WITH SOURCE CODE
+# REVISE PYTHON TUT 1
+
+print('hello sadiq ali!')
+#single line comment
+"""double 
+line comment"""
+#lets check the python version 
+import sys    
+print('python: ' + sys.version)
+print(sys.winver)
+print(sys.gettrace)
+print(sys.argv)
+print('\n\n')
+#help(sys)  # the python help function is used to display the documentation 
+# modules and here the module name is sys
+
+#STRING
+print('the sadiq ali youtube channel ')
+#lets do the indentation in a wrong way.
+#indentation matter the most in python 
+print(' Hello World! ')
+#frint(' Hello World! ')
+# print(' Hello World!')
+
+#DATA TYPES 
+#lets talk about basic data types 
+print('\n BASIC DATA TYPES AND STRINGS')
+#integer
+print(1234567890)
+#float
+print(1.9)
+#Bolean 
+print(True)
+print(False)
+print(bool(1))
+print(bool(0))
+
+#int
+print(type(1234))
+print(type(-1234))
+print(type(0))
+print(type(1))
+
+#float
+
+print(type(0.1))
+print(type(1.1))
+print(type(9.0))
+print(type(-1.0))
+
+#bolean
+
+print(type(True))
+print(type(False))
+
+
+#you can add space between to output like this also
+print("sadiq")
+print()
+print('ali')
+
+#"" '' Are same
+
+#converting an object type to another object types 
+#lets do this 
+
+print("\nlets convert integer number to a string and float.\n")
+
+number = 1
+print(number)
+print(str(number))
+print(float(number))
+print(type(number))
+print(type(float(number)))
+str(number)
+print(type(str(number)))
+
+#you can zoom in and zoom out .  shift plus ctrl plus + and shift plus ctrl plus -
+
+#let convert boolean to an integer
+print("\n Boolean conversion: ")
+bool_1 = True
+bool_2 = False
+print(int(bool_1))
+print(float(bool_1))
+print(str(bool_1))
+print(int(bool_2))
+print(float(bool_2))
+print(str(bool_2))
+
+
+#lets find the data type of 1/2
+print(1/2)
+print(type(1/2))
+print(type(2/1))
+print(type(1/1))
+
+#FOR TODAY THATS IT, TOMMOROW WE WILL BE DOING ADDING AND SUBTRACTING TWO VALUES
+#EXPRESSION AND VARIABLES
+#BYE BYE 
+#THANK YOU SOO MUCH FOR WATCHING THIS VIDEO
+#DO LIKE AND SUBSCRIBE
+
+
+ 
+
+
+
