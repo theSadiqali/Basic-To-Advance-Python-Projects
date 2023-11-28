07c771b78e6c0f180f3997daca24bf18b2222192
Sadiq Ali <142330669+theSadiqali@users.noreply.github.com>
2023-11-28 20:13:20 +0500

    Python Tutorial #4.py

---
+++ Python Tut 4.py
@@ -0,0 +1,180 @@
+                                    #=========================#
+                                    #        Tutorial 4       #
+                                    #-------------------------#
+                                    #        YOUTUBE          #
+                                    #   thesadiqali channel   #
+                                    #  ====================== # 
+                                    #   PYTHON Tutorial       #
+                                    #   BASIC TO ADVANCE      #
+                                    #=========================#
+            
+# Expression, Variable and String 
+
+#Addition
+a = 1
+b = 2
+c = a+b
+print(f"C: {c}")
+
+sum = 1+2+3+4
+print(f"Sum: {sum}")
+print(type(sum))
+
+Result = 1.0 + 2
+print(f"Result:  {Result}")
+print(type(Result))
+
+#Subtraction
+print("\n\n")
+
+sub = 1 - 2 - 3
+print(f"Subtraction: {sub}")
+print(type(sub))
+
+#Float
+sub = 1 - 2 - 3.0
+print(f"Subtraction: {sub}")
+print(type(sub))
+
+e = 1
+f = 2
+Subtraction1 = e-f
+print(f"Subtraction: {Subtraction1}")
+print(type(Subtraction1))
+#Let See 
+print("\n")
+#Multiplication 
+Mul1 = 1
+Mul2 = 3
+Multiplication = Mul1 * Mul2
+print(f"Multiplication: {Multiplication}")
+print(type(Multiplication))
+
+#Division 
+print("\n")
+div1 = 1
+div2 = 3
+Division = div1 / div2
+print(f"Division: {Division}")
+print(type(Division))
+
+
+# Floor Divsion 
+# I just forgot . 
+print("\n")
+
+floor1 = 45
+floor2 = 123
+FloorDivsion = floor1 // floor2
+
+print(f"Floor: {FloorDivsion}")
+print(type(FloorDivsion))
+
+
+#now Modulus 
+#Modulus
+print("\n")
+mod1 = 100
+mod2 = 150
+modulus = mod1 % mod2
+print(f"Modulus: {modulus}")
+print(type(modulus))
+
+#Exponential 
+Exponential = 2**4
+print(f"Exponential: {Exponential}")
+print(type(Exponential))
+
+#Now lets see at example 
+print("\n\n")
+print("Example1")
+#An example lets calculate how many minutes there are in 20 hours?
+one_hour = 60  # has 60 minutes
+hour = 20 
+minutes = one_hour * hour
+print(f"Minutes: {minutes}")
+print(type(minutes))
+
+#An example lets calculate how many hours are there are in 348 minutes?
+print("\n\n")
+print("Example2")
+minutes = 348
+one_hour_ = 60
+hours = 348/60 #OR use the variable
+print(f"Hours: {hours}")
+print(type(hours))
+
+#it would be better if we just make them a good look 
+#Lets write all the mathematics expressions
+print("\n")
+print("Mathematics Expressions: ")
+var1 = 1 * 2 + 3
+var2 = 1*(2+3)
+print(var1)
+print(var2)
+print(var1 + var2)
+print(var1 - var2)
+print(var1 * var2)
+print(var1 / var2)
+print(var1 // var2)
+print(var1 ** var2)
+print(var1 % var2)
+
+#Variable
+print("\n")
+print("Store the value in the Variable:")
+Variable = 1.0 
+print(f"Variable: {Variable}")
+print(type(Variable))
+
+#And You Know values of variable can be writte in one line 
+v1, v2, v3, v4 = 1,2,3,4
+print(f"v1,v2,v3,v4: {v1,v2,v3,v4}")
+
+
+Var = 1,2,3,4
+Var = v1, v2, v3, v4 
+print(f"v1,v2,v3,v4: {v1,v2,v3,v4}")
+
+#lets add String
+#String
+print("\n")
+print("STRING")
+
+print("Hello World!") #Double qoutation marks for describing a string
+#single qoutations 'Hello World!'
+
+#Digits and Spaces
+print("1 2 3 4 5 6") #we know the output
+
+#specific Character 
+print('~1@#$%^&*()')
+
+string = "The Sadiq Ali"
+print(f"print the message: {string}")
+string
+print(string[2])
+print(f"Length of the String: {len(string)}")
+
+#lets do the negative indexing
+print(f"Last character using negative indexing: {string[-1]}")
+
+#Slicing of a string
+print(f"Slicing of a String: {string[0:6]}") 
+
+#Striding in A string
+print(f"Striding in A string: {string[::4]}")
+#Slicing the Escape String
+print("Escape the STRING \n\n THE SADIQ ALI")
+print("Tab Escape the STRING \t\t THE SADIQ ALI")
+
+#String Operator
+
+string_upper = string.upper()
+print("thesadiqali String Operator ",string.upper)
+print("thesadiqali String Operator ",string.lower())
+
+#there a lots of other you can practice with
+#just like replace and lower method OR find methods
+#SO FOR TODAY THATS IT. BYE BYE 
+#THANK U SOO MUCH FOR WATCHING THIS VIDEO 
